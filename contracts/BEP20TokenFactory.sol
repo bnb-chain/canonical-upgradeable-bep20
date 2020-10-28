06a3798d5f26285f7d660c67b0e1ba693621f349
HaoyangLiu <lhy.0318@163.com>
2020-10-28 11:05:25 +0800

    remove useless method from interface

--- contracts/BEP20TokenFactory.sol
+++ contracts/BEP20TokenFactory.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.6.0;
 
 import './BEP20UpgradeableProxy.sol';
-import './IProxyBEP20.sol';
+import './IProxyInitialize.sol';
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
 contract BEP20TokenFactory is Ownable{
@@ -17,7 +17,7 @@ contract BEP20TokenFactory is Ownable{
     function createBEP20Token(string calldata name, string calldata symbol, uint8 decimals, uint256 amount, bool mintable, address bep20Owner, address proxyAdmin) external onlyOwner returns (address) {
         BEP20UpgradeableProxy proxyToken = new BEP20UpgradeableProxy(logicImplement, proxyAdmin, "");
 
-        IProxyBEP20 token = IProxyBEP20(address(proxyToken));
+        IProxyInitialize token = IProxyInitialize(address(proxyToken));
         token.initialize(name, symbol, decimals, amount, mintable, bep20Owner);
         emit TokenCreated(address(token));
         return address(token);
